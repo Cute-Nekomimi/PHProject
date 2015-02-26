a7c5c74b9a29f0d892a5b1fa1917d70cf69d98d0
Jordan Raynard <raynard.jordan@gmail.com>
2015-02-26 06:19:28 -0400

    Course validation in PHP
    Included check to make sure that 4 courses are selected and all are
    valid

--- form_submit.php
+++ form_submit.php
@@ -19,7 +19,26 @@
   $b_emailExists = preg_match("/\S+/",$ass_values["email"]);
   $b_dobExists = preg_match("/\S+/",$ass_values["date-of-birth"]);
 
-  
+  $asi_course1 = explode(":",$ass_values["course-1"],2);
+  $asi_course2 = explode(":",$ass_values["course-2"],2);
+  $asi_course3 = explode(":",$ass_values["course-3"],2);
+  $asi_course4 = explode(":",$ass_values["course-4"],2);
+
+  $sql_query = $db->prepare("SELECT count(*) FROM `course` WHERE (`name`=:name1 AND `grade`=:grade1) OR (`name`=:name2 AND `grade`=:grade2) OR (`name`=:name3 AND `grade`=:grade3) OR (`name`=:name4 AND `grade`=:grade4);");
+  $sql_query->bindParam(":name1",$asi_course1[0]);
+  $sql_query->bindParam(":grade1",$asi_course1[1]);
+  $sql_query->bindParam(":name2",$asi_course2[0]);
+  $sql_query->bindParam(":grade2",$asi_course2[1]);
+  $sql_query->bindParam(":name3",$asi_course3[0]);
+  $sql_query->bindParam(":grade3",$asi_course3[1]);
+  $sql_query->bindParam(":name4",$asi_course4[0]);
+  $sql_query->bindParam(":grade4",$asi_course4[1]);
+
+  $sql_query->execute();
+  $result = $sql_query->fetchColumn();
+
+
+  $b_correctCourseCount = ($result==4);
 ?>
 
 <!DOCTYPE html>
@@ -30,12 +49,7 @@
   </head>
   <body>
     <?php 
-      if ($firstNameExists) {
-        echo "True";
-      }
-      else {
-        echo "False";
-      }
+      echo $result;
      ?>
   </body>
 </html>
